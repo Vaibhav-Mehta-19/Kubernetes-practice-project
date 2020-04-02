9e4d4494c0835205c1705108dd1c4b7d981906ac
pradeep <pradeep.kumar44@gmail.com>
2020-04-02 08:50:03 +0530

    commmitting the changes

--- docker-files/talking-cow/talkingcow.sh
+++ docker-files/talking-cow/talkingcow.sh
@@ -3,6 +3,6 @@ trap "exit" SIGINT
 while :
 do
   echo $(date) Writing fortune to /var/htdocs/index.html
-  /usr/games/fortune > /var/htdocs/index.html
+  fortune > /var/htdocs/index.html
   sleep 10
 done
